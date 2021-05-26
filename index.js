e3592f96cfcd569761710a16b965d4e127dc3178
Phavyatkr <phavya@getchaine.com>
2021-05-26 13:51:15 +0200

    Level 2 changes made

---
+++ index.js
@@ -0,0 +1,9 @@
+let baby = 'Phav';
+console.log(baby);
+
+let firstName = "Phavya";
+let lastName = "Chathurthi";
+
+const interestRate = 0.5;
+interestRate = 1;
+console.log(interestRate);
--- pratice.js
+++ pratice.js
@@ -1,8 +1,66 @@
-const { workerData } = require("worker_threads")
 
-This is practice file
-editing one more line in local
-Adding one more line from github
-checking one more time from local to global
-this is from github to local need to update this in local also
-wow worked
+function greet(firstName)
+{
+  
+return `Hello ${firstName}`
+
+}
+
+function logger(name)
+{
+console.log(greet(name))
+}
+function greetAndLog(friend)
+{
+  logger(friend)
+}
+greetAndLog("Ada")
+
+
+function day2(weekName)
+{
+  return `Today is ${weekName}`
+ 
+}
+function whatDay(Weekname)
+{
+  console.log(day2(weekName))
+}
+ function dayLog(weekName)
+ {
+   day2(weekName)
+   whatDay(weekName)
+ }
+dayLog("Monday")
+
+dayLog("Tuesday")
+
+function add(a, b)
+{
+  console.log(a+b)
+
+}
+add(1,2)
+add(3,4)
+add("f", "s")
+
+//NEED TO CHECK WITH MADHAV 
+
+function day2(name)
+{
+  return `Today is ${name}`
+ 
+}
+
+
+function whatDay(name)
+{
+  console.log(day2(name))
+}
+ day2('Wednesday')
+
+whatDay("Monday")
+
+whatDay("Tuesday")
+
+
